20276851714ed295117235af4e185e9b9f5e6736
Jademir de Moura <jademirf@gmail.com>
2022-11-07 10:31:45 -0300

    add main navigation

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -2,9 +2,12 @@ import { VStack, Divider, Icon } from "native-base";
 import { Octicons } from "@expo/vector-icons"
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 
 export function Pools() {
+  const navigation = useNavigation()
+
   return(
     <VStack flex={1} bgColor="gray.900">
       <Header title="Meus bolões" />
@@ -13,6 +16,7 @@ export function Pools() {
           title="BUSCAR BOLÃO POR CÓDIGO"
           mt={8}
           leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
+          onPress={() => navigation.navigate('find')}
         />
         <Divider 
           my="4" 
